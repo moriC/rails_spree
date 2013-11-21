8cf141c526c594874b34365012e49b1e8bd32b7f
morishita <tomohiro.m0219@gmail.com>
2013-11-21 22:45:05 +0900

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+RailsSpree::Application.config.secret_key_base = '0ad70fdb6756e1a95dad58071e7d23070d9fdaba38d8c7688115c866567af6f1d34c70e27df7606f9a3d8f56a12a3f20a0d131099583688c3a5bfc2fc39c050d'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+RailsSpree::Application.config.session_store :cookie_store, key: '_rails_spree_session'
